7d574335f6d3f96fce2f0604db64aab6f9115b50
thanhvyhcmus <thanhvy.forwork@gmail.com>
2020-11-02 22:05:13 +0700

    Coming-soon Section
    Comingsoon content and tab javascript file

---
+++ ticketbox/movie.js
@@ -0,0 +1,19 @@
+function openTabs(evt, classify) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(classify).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+document.getElementById("default").click();
